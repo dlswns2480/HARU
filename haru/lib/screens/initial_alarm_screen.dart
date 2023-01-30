f4641cc7a4177960bd74c4bbbac7da8b73d91821
20192931 <gowhidol2@gmail.com>
2023-01-30 21:12:12 +0900

    Make Initial Alarm Page2

--- haru/lib/screens/initial_alarm_screen.dart
+++ haru/lib/screens/initial_alarm_screen.dart
@@ -1,1 +1,55 @@
+import 'package:flutter/material.dart';
 
+class MyWidget extends StatefulWidget {
+  const MyWidget({super.key});
+
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 3,
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+        title: const Text(
+          "카테고리 선택",
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 7,
+            vertical: 80,
+          ),
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 50,
+              ),
+              SizedBox(
+                width: 350,
+                height: 50,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.black),
+                  ),
+                  child: const Text('다음'),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
