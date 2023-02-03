7ef9ab695729f3d87169e14761dcd9157cf585ef
dlswns2480 <dlswns2480@naver.com>
2023-02-03 11:57:51 +0900

    처음카테고리 화면이랑 처음알람 화면에서 뒤로가기 못하게함

--- haru/lib/screens/initial_alarm_screen.dart
+++ haru/lib/screens/initial_alarm_screen.dart
@@ -11,49 +11,53 @@ class InitialAlarm extends StatefulWidget {
 class InitialAlarmtState extends State<InitialAlarm> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        elevation: 3,
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: Scaffold(
         backgroundColor: Colors.white,
-        foregroundColor: Colors.black,
-        title: const Text(
-          "알림 시간",
-          style: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.w600,
+        appBar: AppBar(
+          elevation: 3,
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.black,
+          title: const Text(
+            "알림 시간",
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.w600,
+            ),
           ),
+          automaticallyImplyLeading: false,
         ),
-      ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(
-            horizontal: 40,
-            vertical: 100,
-          ),
-          child: Column(
-            children: [
-              const SizedBox(
-                height: 400,
-              ),
-              SizedBox(
-                width: 350,
-                height: 50,
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const HomeScreen()),
-                    );
-                  },
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(Colors.black),
-                  ),
-                  child: const Text('다음'),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 40,
+              vertical: 100,
+            ),
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 400,
                 ),
-              ),
-            ],
+                SizedBox(
+                  width: 350,
+                  height: 50,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const HomeScreen()),
+                      );
+                    },
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.black),
+                    ),
+                    child: const Text('다음'),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- haru/lib/screens/initial_category_screen.dart
+++ haru/lib/screens/initial_category_screen.dart
@@ -59,100 +59,104 @@ class _InitialCategorySelectState extends State<InitialCategorySelect> {
   //bool check = false;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        elevation: 3,
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: Scaffold(
         backgroundColor: Colors.white,
-        foregroundColor: Colors.black,
-        title: const Text(
-          "카테고리 선택",
-          style: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.w600,
+        appBar: AppBar(
+          elevation: 3,
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.black,
+          title: const Text(
+            "카테고리 선택",
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.w600,
+            ),
           ),
+          automaticallyImplyLeading: false,
         ),
-      ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(
-            horizontal: 7,
-            vertical: 80,
-          ),
-          child: Column(
-            children: [
-              GridView.count(
-                shrinkWrap: true,
-                crossAxisCount: 3,
-                children: List.generate(9, (index) {
-                  return Center(
-                    child: Column(
-                      children: [
-                        InkWell(
-                          onTap: () {
-                            setState(() {
-                              _selectedCategorys[index] =
-                                  !_selectedCategorys[index];
-                            });
-                          },
-                          child: Container(
-                            width: 90,
-                            height: 90,
-                            decoration: BoxDecoration(
-                              image: DecorationImage(
-                                image: AssetImage(
-                                  categoryimage[index],
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 7,
+              vertical: 80,
+            ),
+            child: Column(
+              children: [
+                GridView.count(
+                  shrinkWrap: true,
+                  crossAxisCount: 3,
+                  children: List.generate(9, (index) {
+                    return Center(
+                      child: Column(
+                        children: [
+                          InkWell(
+                            onTap: () {
+                              setState(() {
+                                _selectedCategorys[index] =
+                                    !_selectedCategorys[index];
+                              });
+                            },
+                            child: Container(
+                              width: 90,
+                              height: 90,
+                              decoration: BoxDecoration(
+                                image: DecorationImage(
+                                  image: AssetImage(
+                                    categoryimage[index],
+                                  ),
                                 ),
+                                borderRadius: BorderRadius.circular(15),
+                                border: Border.all(
+                                    color: _selectedCategorys[index]
+                                        ? Colors.blue
+                                        : Colors.black,
+                                    width: 3.5),
                               ),
-                              borderRadius: BorderRadius.circular(15),
-                              border: Border.all(
-                                  color: _selectedCategorys[index]
-                                      ? Colors.blue
-                                      : Colors.black,
-                                  width: 3.5),
+                              // child: Icon(
+                              //   size: 50,
+                              //   icon_list[index],
+                              //   color: _selectedCategorys[index]
+                              //       ? Colors.blue
+                              //       : Colors.black,
+                              // ),
                             ),
-                            // child: Icon(
-                            //   size: 50,
-                            //   icon_list[index],
-                            //   color: _selectedCategorys[index]
-                            //       ? Colors.blue
-                            //       : Colors.black,
-                            // ),
                           ),
-                        ),
-                        Text(
-                          categoryItem[index],
-                          style: const TextStyle(
-                            fontSize: 15,
-                            fontWeight: FontWeight.w700,
+                          Text(
+                            categoryItem[index],
+                            style: const TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.w700,
+                            ),
                           ),
-                        ),
-                      ],
-                    ),
-                  );
-                }),
-              ),
-              const SizedBox(
-                height: 50,
-              ),
-              SizedBox(
-                width: 350,
-                height: 50,
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const InitialAlarm()),
+                        ],
+                      ),
                     );
-                  },
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(Colors.black),
-                  ),
-                  child: const Text('다음'),
+                  }),
                 ),
-              ),
-            ],
+                const SizedBox(
+                  height: 50,
+                ),
+                SizedBox(
+                  width: 350,
+                  height: 50,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const InitialAlarm()),
+                      );
+                    },
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.black),
+                    ),
+                    child: const Text('다음'),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
