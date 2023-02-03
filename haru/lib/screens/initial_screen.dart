04c98993f03f60003f2c0c428698aebdf27e38f9
dlswns2480 <dlswns2480@naver.com>
2023-02-03 11:45:50 +0900

    초기화면에 앱 이름 띄움

--- haru/lib/screens/initial_screen.dart
+++ haru/lib/screens/initial_screen.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:haru/screens/initial_category_screen.dart';
 
@@ -24,46 +23,47 @@ class _InitialScreenState extends State<InitialScreen> {
 
   @override
   Widget build(BuildContext context) {
-    const String imageLogoName = 'images/atom.png';
-
-    var screenHeight = MediaQuery.of(context).size.height;
-    var screenWidth = MediaQuery.of(context).size.width;
-
     return WillPopScope(
       onWillPop: () async => false,
       child: MediaQuery(
         data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
         child: const Scaffold(
-            backgroundColor: Colors.white, body: Text("aasdasd")
-            //Container(
-            //   //height : MediaQuery.of(context).size.height,
-            //   //color: kPrimaryColor,
-            //   child: Column(
-            //     mainAxisAlignment: MainAxisAlignment.start,
-            //     children: <Widget>[
-            //       SizedBox(height: screenHeight * 0.384375),
-            //       Container(
-            //         child: SvgPicture.asset(
-            //           imageLogoName,
-            //           width: screenWidth * 0.616666,
-            //           height: screenHeight * 0.0859375,
-            //         ),
-            //       ),
-            //       const Expanded(child: SizedBox()),
-            //       Align(
-            //         child: Text("© Copyright 2020, 내방니방(MRYR)",
-            //             style: TextStyle(
-            //               fontSize: screenWidth * (14 / 360),
-            //               color: const Color.fromRGBO(255, 255, 255, 0.6),
-            //             )),
-            //       ),
-            //       SizedBox(
-            //         height: MediaQuery.of(context).size.height * 0.0625,
-            //       ),
-            //     ],
-            //   ),
-            // ),
+          backgroundColor: Colors.white,
+          body: Text(
+            "하루지식",
+            style: TextStyle(
+              fontSize: 50,
             ),
+            textAlign: TextAlign.center,
+          ),
+          //Container(
+          //height : MediaQuery.of(context).size.height,
+          //color: kPrimaryColor,
+          //     Column(
+          //   mainAxisAlignment: MainAxisAlignment.start,
+          //   children: <Widget>[
+          //     SizedBox(height: screenHeight * 0.384375),
+          //     Container(
+          //       child: SvgPicture.asset(
+          //         imageLogoName,
+          //         width: screenWidth * 0.616666,
+          //         height: screenHeight * 0.0859375,
+          //       ),
+          //     ),
+          //     const Expanded(child: SizedBox()),
+          //     Align(
+          //       child: Text("© Copyright 2020, 내방니방(MRYR)",
+          //           style: TextStyle(
+          //             fontSize: screenWidth * (14 / 360),
+          //             color: const Color.fromRGBO(255, 255, 255, 0.6),
+          //           )),
+          //     ),
+          //     SizedBox(
+          //       height: MediaQuery.of(context).size.height * 0.0625,
+          //     ),
+          //   ],
+          // ),
+        ),
       ),
     );
   }
