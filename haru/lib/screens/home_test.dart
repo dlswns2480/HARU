c640a440ecb09e1adbc824972f70e2c052b95e71
강승민 <rkdtmdals999@naver.com>
2023-02-06 13:12:42 +0900

    폰트 추가 및 홈 화면 레이아웃 변경 (이제 지식 텍스트 부분 스크롤 가능, 타이틀 텍스트 추가, 명언 입력 시 이름 입력 칸 추가)

--- haru/lib/screens/home_test.dart
+++ haru/lib/screens/home_test.dart
@@ -13,169 +13,212 @@ class HomeTest extends StatefulWidget {
 class _HomeTestState extends State<HomeTest> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        physics: const NeverScrollableScrollPhysics(),
-        child: Column(
-          mainAxisSize: MainAxisSize.max,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              width: double.infinity,
-              height: MediaQuery.of(context).size.height * 1,
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                  fit: BoxFit.cover,
-                  image: NetworkImage(
-                    'https://images.unsplash.com/photo-1569769107543-e0f61bab8e02?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80',
-                  ),
+    return const Scaffold(
+      body: HomeKnowledgeWidget(
+        title: "경제",
+        imagePath:
+            "https://images.unsplash.com/photo-1526304640581-d334cdbbf45e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80",
+        knowledge:
+            "그레이 캐피털리즘(Gray Capitalism)이란 정부 통제력이 강한 기존 중국식 자본주의에서 통제를 풀고 시장을 확대하는 과정에 있는 중간적 형태의 자본주 의를 뜻한다.",
+        author: "null",
+      ),
+    );
+  }
+}
+
+@immutable
+class HomeKnowledgeWidget extends StatelessWidget {
+  final String title;
+  final String imagePath;
+  final String knowledge;
+  final String author;
+
+  const HomeKnowledgeWidget({
+    super.key,
+    required this.title,
+    required this.imagePath,
+    required this.knowledge,
+    required this.author,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      physics: const NeverScrollableScrollPhysics(),
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            width: double.infinity,
+            height: MediaQuery.of(context).size.height * 1,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: NetworkImage(
+                  imagePath,
                 ),
               ),
-              child: Container(
-                width: 100,
-                height: 10,
-                decoration: const BoxDecoration(
-                  color: Color(0x59000000),
-                ),
-                child: Column(
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding:
-                          const EdgeInsetsDirectional.fromSTEB(24, 70, 24, 44),
-                      child: Column(
-                        mainAxisSize: MainAxisSize.max,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const Text(
-                            '오늘의 지식',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontFamily: "NanumSquareRound",
-                              fontWeight: FontWeight.w400,
-                              fontSize: 30,
-                            ),
-                            // style: FlutterFlowTheme.of(context).title1.override(
-                            //       fontFamily: 'Poppins',
-                            //       color: Colors.white,
+            ),
+            child: Container(
+              width: 100,
+              height: 10,
+              decoration: const BoxDecoration(
+                color: Color(0x59000000),
+              ),
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding:
+                        const EdgeInsetsDirectional.fromSTEB(24, 70, 24, 44),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.max,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          '오늘의 지식',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontFamily: "NanumSquareRound",
+                            fontWeight: FontWeight.w400,
+                            fontSize: 30,
                           ),
-                          Padding(
-                            padding: const EdgeInsetsDirectional.fromSTEB(
-                                0, 4, 0, 0),
-                            child: Text(
-                              '${dt.year}.${dt.month}.${dt.day}',
-                              style: const TextStyle(
-                                color: Colors.white70,
-                                fontSize: 15,
-                              ),
-
-                              // style: FlutterFlowTheme.of(context)
-                              //     .bodyText2
-                              //     .override(
-                              //       fontFamily: 'Poppins',
-                              //       color: const Color(0xFFD5DBDF),
+                        ),
+                        Padding(
+                          padding:
+                              const EdgeInsetsDirectional.fromSTEB(0, 4, 0, 0),
+                          child: Text(
+                            '${dt.year}.${dt.month}.${dt.day}',
+                            style: const TextStyle(
+                              color: Colors.white70,
+                              fontSize: 15,
                             ),
                           ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
-                    Expanded(
-                      child: ClipRRect(
-                        child: BackdropFilter(
-                          filter: ImageFilter.blur(
-                            sigmaX: 6,
-                            sigmaY: 7,
+                  ),
+                  Expanded(
+                    child: ClipRRect(
+                      child: BackdropFilter(
+                        filter: ImageFilter.blur(
+                          sigmaX: 6,
+                          sigmaY: 7,
+                        ),
+                        child: Container(
+                          width: double.infinity,
+                          height: MediaQuery.of(context).size.height * 0.4,
+                          decoration: const BoxDecoration(
+                            color: Color(0x87000000),
                           ),
-                          child: Container(
-                            width: double.infinity,
-                            height: MediaQuery.of(context).size.height * 0.4,
-                            decoration: const BoxDecoration(
-                              color: Color(0x87000000),
-                            ),
-                            child: Align(
-                              alignment: const AlignmentDirectional(0, 0),
-                              child: Padding(
-                                padding: const EdgeInsetsDirectional.fromSTEB(
-                                    24, 24, 24, 24),
-                                child: SingleChildScrollView(
-                                  child: Column(
-                                    mainAxisSize: MainAxisSize.max,
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: [
-                                      Row(
-                                        mainAxisSize: MainAxisSize.max,
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.center,
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        children: const [
-                                          Text(
-                                            '"Think Different"',
-                                            style: TextStyle(
-                                              color: Colors.white,
-                                              fontSize: 30,
-                                              fontFamily: 'NanumSquare',
-                                              fontWeight: FontWeight.w300,
-                                            ),
-                                            // style: FlutterFlowTheme.of(context).bodyText1.override(
-                                            //       fontFamily: 'Poppins',
-                                            //       color: Colors.white,
-                                            //       fontSize: 25,
-                                            //     ),
-                                          ),
-                                        ],
+                          child: Align(
+                            alignment: const AlignmentDirectional(0, 0),
+                            child: Padding(
+                              padding: const EdgeInsetsDirectional.fromSTEB(
+                                  24, 24, 24, 24),
+                              child: SingleChildScrollView(
+                                child: Column(
+                                  mainAxisSize: MainAxisSize.max,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text(
+                                      title,
+                                      style: const TextStyle(
+                                        fontFamily: "NanumSquare",
+                                        fontWeight: FontWeight.w700,
+                                        fontSize: 40,
+                                        color: Colors.white,
                                       ),
-                                      Container(
-                                        width:
-                                            MediaQuery.of(context).size.width *
-                                                0.9,
-                                        height: 2,
-                                        decoration: const BoxDecoration(
-                                          color: Color(0x34FFFFFF),
+                                    ),
+                                    const SizedBox(
+                                      height: 100,
+                                    ),
+                                    SizedBox(
+                                      width: 400,
+                                      height: 300,
+                                      child: SingleChildScrollView(
+                                        scrollDirection: Axis.vertical,
+                                        child: Row(
+                                          mainAxisSize: MainAxisSize.max,
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.start,
+                                          // crossAxisAlignment:
+                                          //     CrossAxisAlignment.start,
+                                          children: [
+                                            Flexible(
+                                              child: RichText(
+                                                overflow: TextOverflow.ellipsis,
+                                                maxLines: 100,
+                                                strutStyle: const StrutStyle(
+                                                    fontSize: 16.0),
+                                                text: TextSpan(
+                                                  text: knowledge,
+                                                  style: const TextStyle(
+                                                    color: Colors.white,
+                                                    fontSize: 25,
+                                                    fontFamily: 'NanumSquare',
+                                                    fontWeight: FontWeight.w300,
+                                                  ),
+                                                ),
+                                              ),
+                                            ),
+                                          ],
                                         ),
                                       ),
-                                      Row(
-                                        mainAxisSize: MainAxisSize.max,
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.end,
-                                        children: const [
-                                          Icon(
-                                            Icons.person,
-                                            color: Colors.white,
-                                            size: 20,
-                                          ),
-                                          Text(
-                                            'Steve Jobs',
-                                            style: TextStyle(
-                                                color: Colors.white54,
-                                                fontFamily: 'NanumSquare',
-                                                fontWeight: FontWeight.w300,
-                                                fontSize: 20),
-                                            // style: FlutterFlowTheme.of(context).bodyText1.override(
-                                            //       fontFamily: 'Poppins',
-                                            //       color: Colors.white,
-                                            //       fontSize: 20,
-                                            //     ),
-                                          ),
-                                        ],
+                                    ),
+                                    const SizedBox(
+                                      height: 10,
+                                    ),
+                                    Container(
+                                      width: MediaQuery.of(context).size.width *
+                                          0.9,
+                                      height: 2,
+                                      decoration: const BoxDecoration(
+                                        color: Color(0x34FFFFFF),
                                       ),
-                                    ],
-                                  ),
+                                    ),
+                                    const SizedBox(
+                                      height: 10,
+                                    ),
+                                    Row(
+                                      mainAxisSize: MainAxisSize.max,
+                                      mainAxisAlignment: MainAxisAlignment.end,
+                                      children: [
+                                        const Icon(
+                                          Icons.person,
+                                          color: Colors.white,
+                                          size: 20,
+                                        ),
+                                        (author != "null")
+                                            ? Text(
+                                                author,
+                                                style: const TextStyle(
+                                                    color: Colors.white54,
+                                                    fontFamily:
+                                                        'NanumGothicEco',
+                                                    fontWeight: FontWeight.w300,
+                                                    fontSize: 20),
+                                              )
+                                            : const Text(""),
+                                      ],
+                                    ),
+                                  ],
                                 ),
                               ),
                             ),
                           ),
                         ),
                       ),
-                    )
-                  ],
-                ),
+                    ),
+                  )
+                ],
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
