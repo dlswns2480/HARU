674dfaeb6e925a6cc7958b335cb2041af824170e
dlswns2480 <dlswns2480@naver.com>
2023-02-16 15:21:39 +0900

    firebase 연동

--- haru/lib/main.dart
+++ haru/lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:haru/screens/initial_screen.dart';
+import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
